2f275f7ba0e01979751006142839d28137a657eb
JIMYUNG_YU <jm_7516@naver.com>
2024-10-15 21:48:50 +0900

    241015 웹개발 강의 - JAVA 변수와 자료형

---
+++ JAVA/실습/day01/src/exam01/Ex01.java
@@ -0,0 +1,12 @@
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int num; // 변수 선언 : 값을 담을 메모리 생성(공간 할당)
+        num = 10; //선언한 변수에 값을 저장(대입). 최초로 값을 저장한것 : 초기화
+                  //오른쪽에 있는 10이라는 값을 num이라는 변수에 저장
+        int num1 = 20; //선언과 동시에 초기화도 가능
+                       //초기화 : 변수에 최초로 값을 저장하는 것
+        System.out.println(num);
+    }
+}
