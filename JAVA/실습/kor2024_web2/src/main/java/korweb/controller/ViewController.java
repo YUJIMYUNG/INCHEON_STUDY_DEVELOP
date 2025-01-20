450cf32270f4f718a787578a336ef9bd3008314d
JIMYUNG_YU <jm_7516@naver.com>
2025-01-20 21:47:49 +0900

    0120 웹개발강의 - Java(spring) 부트스트랩으로 css 꾸미기

---
+++ JAVA/실습/kor2024_web2/src/main/java/korweb/controller/ViewController.java
@@ -0,0 +1,24 @@
+package korweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("")
+    // http://localhost:8080/
+    public String index(){
+        return "/index.html";
+    }
+
+    @GetMapping("/member/signup")
+    public String signup() {
+        return "/member/signup.html";
+    }
+
+    @GetMapping("/member/login")
+    public String login(){
+        return "/member/login.html";
+    }
+}
